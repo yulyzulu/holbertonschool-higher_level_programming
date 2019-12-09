e03d6de9ac5df097d447fdb21c06f37281d5c97c
yulyzulu <yulyzulu05@gmail.com>
2019-12-09 22:59:12 +0000

    linked lsit cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+*
+*
+*
+*
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *current;
+	listint_t *prox;
+
+	current = list;
+	prox = list;
+	
+	while (current && prox)
+	{
+		current = current->next;
+		prox = prox->next->next;
+
+		if (current == prox)
+		{
+			return (1);
+
+		}
+	}
+	return (0);
+}
