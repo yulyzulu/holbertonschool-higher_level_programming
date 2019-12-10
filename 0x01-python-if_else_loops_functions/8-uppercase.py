2f19250ab85e4b87c25d62e3397a331582b63299
yulyzulu <yulyzulu05@gmail.com>
2019-12-10 23:07:10 +0000

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+
+    for i in str:
+        if (ord(i) >= 97 and ord(i) <= 123):
+            i = (ord(i) - 32)
+        else:
+            i = ord(i)
+        print("{:c}".format(i), end="")
+    print("")
