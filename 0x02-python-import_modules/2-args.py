f57b10316df7b955da4f64967682190fd554d78e
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-12-12 19:03:06 +0000

    line to import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import sys
-numlen = len(sys.argv)
-if numlen == 1:
-    print("0 arguments.")
-elif numlen == 2:
-    print("1 argument:")
-    print("1: ", sys.argv[1])
-else:
-    print("{:d} arguments:".format(numlen - 1))
-    for i in range(numlen):
-        if i != 0:
-            print("{:d}: ".format(i), sys.argv[i])
+if __name__ == "__main__":
+    import sys
+    numlen = len(sys.argv)
+    if numlen == 1:
+        print("0 arguments.")
+    elif numlen == 2:
+        print("1 argument:")
+        print("1: ", sys.argv[1])
+    else:
+        print("{:d} arguments:".format(numlen - 1))
+        for i in range(numlen):
+            if i != 0:
+                print("{:d}: ".format(i), sys.argv[i])
