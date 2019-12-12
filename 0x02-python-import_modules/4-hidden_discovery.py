d8e08525f97d420b155bc5b5970d96de499fe782
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-12-12 22:55:28 +0000

    dir

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-
+if __name__ == "__main__":
+    import hidden_4.pyc
+    for i in dir(hidden_4):
+        if i[0] != "_" and i[1] != "_":
+            print("{:s}".format(i))
