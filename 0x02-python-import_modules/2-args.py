47614c2369893915328a1d1ba863f4a003f7a8a2
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-12-12 18:59:15 +0000

    print argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 import sys
-
-print(sys.argv[1], sys.argv[2])
+numlen = len(sys.argv)
+if numlen == 1:
+    print("0 arguments.")
+elif numlen == 2:
+    print("1 argument:")
+    print("1: ", sys.argv[1])
+else:
+    print("{:d} arguments:".format(numlen - 1))
+    for i in range(numlen):
+        if i != 0:
+            print("{:d}: ".format(i), sys.argv[i])
