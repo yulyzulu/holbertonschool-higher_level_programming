4f136f479a3e573c985764212ba94997f7123fe8
yulyzulu <yulyzulu05@gmail.com>
2019-12-17 18:04:45 +0000

    leng

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for i in range(len(my_list)):
+    for i in range(len(my_list) - 1):
         if idx < 0:
             return (None)
-        elif (idx > (len(my_list) - 1):
+        elif (idx > (len(my_list) - 1)):
             return (None)
         else:
             return (my_list[idx])
