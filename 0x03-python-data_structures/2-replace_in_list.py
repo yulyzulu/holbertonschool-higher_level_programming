d5b685e133d5d71e8289d51716bca6a773ca3cc5
yulyzulu <yulyzulu05@gmail.com>
2019-12-17 18:10:26 +0000

    length

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@ def replace_in_list(my_list, idx, element):
     for i in range(len(my_list) - 1):
         if idx < 0:
             return (my_list)
-        elif (idx > len(my_list) - 1):
+        elif (idx > len(my_list)):
             return (my_list)
         else:
             my_list[idx] = element
