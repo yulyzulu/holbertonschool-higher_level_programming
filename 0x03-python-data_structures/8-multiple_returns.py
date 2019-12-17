be517c64cb55ca76a1a3cae3c37d3d293d33b53b
yulyzulu <yulyzulu05@gmail.com>
2019-12-17 23:28:08 +0000

    first empty none

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,5 +4,6 @@ def multiple_returns(sentence):
         length = len(sentence)
         first = sentence[0]
     else:
-        first = None
+        length = len(sentence)
+        first = sentence[0] = None
     return (length, first)
