fe5a26e3f68e21c5f48c7fe8b73b9e34824857da
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-12-18 04:05:45 +0000

    delete

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if my_list:
-        if ((idx < 0) or (idx > (len(my_list) - 1))):
+ 
+       if ((idx < 0) or (idx > (len(my_list) - 1))):
             return (my_list)
-        else:
-            for i in range(len(my_list) - 1):
-                del my_list[idx]
-                return (my_list)
+       else:
+           del my_list[idx]
+           return (my_list)
---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+*
+*
+*
+*
+*/
+int is_palindrome(listint_t **head)
+{
+	listint_t temp1, temp2;
+	int i;
+
+	i = 0;
+	while (*head)
+	{
+		head->next;
+		i++;
+	}
+	i = i/2;
+	for (j = 0; 
+
+
+
+}
