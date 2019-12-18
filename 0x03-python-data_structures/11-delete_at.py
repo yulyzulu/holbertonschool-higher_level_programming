7a103f184a1e6b5ed4983104c3eeef910d50ae8c
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-12-18 03:09:23 +0000

    delete index

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -5,5 +5,5 @@ def delete_at(my_list=[], idx=0):
             return (my_list)
         else:
             for i in range(len(my_list)):
-                del my_list[i]
+                del my_list[idx]
                 return (my_list)
