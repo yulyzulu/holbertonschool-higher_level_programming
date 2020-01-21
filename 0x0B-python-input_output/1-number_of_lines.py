0788515e457889a167f8ab883df3d3f48428e744
yulyzulu <yulyzulu05@gmail.com>
2020-01-21 22:27:10 +0000

    filemane

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def number_of_lines(filename=""):
     line_number = 0
-    with open("my_file_0.txt") as f:
+    with open(filename, encoding='utf-8') as f:
         for i in f:
             line_number += 1
     return line_number
