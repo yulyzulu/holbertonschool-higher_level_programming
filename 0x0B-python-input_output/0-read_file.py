cab3fd237c783d2642d81a0214ecb1b4fb2209b2
yulyzulu <yulyzulu05@gmail.com>
2020-01-21 22:18:56 +0000

    mode

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open('my_file_0.txt', encoding = 'utf-8') as f:
+    with open('my_file_0.txt', mode='r', encoding='utf-8') as f:
         for i in f:
             print(i, end="")
