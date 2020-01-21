da2c22942fc92ae8ab98ff99e78297a752c69a32
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-01-21 01:54:07 +0000

    test

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -9,14 +9,22 @@ def text_indentation(text):
         Function that prints a text with 2 new lines
         after each of these characters: ., ? and :
     """
-    i = 0
+
+    cont = 0
+    str1 = ""
+    leng = len(text)
     if type(text) != str:
         raise TypeError("text must be a string")
     else:
-        while i < len(text):
-            if (text[i] == ".") or (text[i] == "?") or (text[i] == ":"):
-                print("{:s}\n".format(text[i]))
-                i += 1
-            else:
-                print("{:s}".format(text[i]), end="")
-            i += 1
+        for i in text:
+            cont = cont + 1;
+            if i != " ":
+                flag = 0
+            if flag == 0:
+                str1 = str1 + i
+            if (i == ".") or (i == "?") or (i == ":"):
+                print("{:s}\n".format(str1))
+                flag = 1
+                str1 = ""
+            if(leng) == cont:
+                print("{:s}".format(str1), end="")
