71544fc207fd4d08db3d28b4e30e1baf13eb56d4
yulyzulu <yulyzulu05@gmail.com>
2020-01-21 23:18:49 +0000

    add all arguments an save with json

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 import json
 
 
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+try:
+    Mylist = load_from_json_file('add_item.json')
+
+except:
+    Mylist = []
+
+finally:
+
+    for i in range(1, len(argv)):
+        Mylist.append(argv[i])
+    save_to_json_file(Mylist, 'add_item.json')
