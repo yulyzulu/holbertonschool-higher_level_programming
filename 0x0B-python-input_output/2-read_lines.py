72ced7bc901b630e6970e8617e0488398a41738b
yulyzulu <yulyzulu05@gmail.com>
2020-01-21 22:28:54 +0000

    filename

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def read_lines(filename="", nb_lines=0):
     cont = 1
-    with open('my_file_0.txt') as f:
+    with open(filename, encoding='utf-8') as f:
         for line in f:
             cont += 1
         if nb_lines <= 0 or nb_lines >= cont:
