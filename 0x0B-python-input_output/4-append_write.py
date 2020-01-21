e4069f0a8d04fd4f5b94ebbfbd537ac83af94b61
yulyzulu <yulyzulu05@gmail.com>
2020-01-21 22:32:13 +0000

    append

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def append_write(filename="", text=""):
-    with open(filename, 'a') as f:
+    with open(filename, mode='a', encoding='utf-8') as f:
         f.write(text)
         return f.write(text)
