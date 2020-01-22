8529f82969aadd3f548ef89bef49ecd5a83ce42d
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-01-22 04:18:36 +0000

    pa

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    triangle = []
+    if n <= 0:
+        return []
+    else:
+        for i in range(n):
+            triangle.append[i]
+        return triangle
