224bb0b511c019d4c1f38859444b1e26ed4aeba0
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-01-22 03:54:34 +0000

    files

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+
+        dictionary = {}
+        for i in attrs:
+            if i in self.__dict__.keys():
+                dictionary[i] = self.__dict__[i]
+        return dictionary
+
+        def reload_from_json(self, json):
+            keys = json.keys()
+            for j in keys:
+                if x in self.__dict__.keys():
+                    self.__dict__[i] = json[i]
