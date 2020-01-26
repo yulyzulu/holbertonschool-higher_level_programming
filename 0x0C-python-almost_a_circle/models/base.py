4a909844c24e8be0d975f5603639b61ad3470e55
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-01-26 20:55:36 +0000

    15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+''' Class Base '''
+import json
+
+
 class Base:
     __nb_objects = 0
     def __init__(self, id=None):
@@ -7,3 +11,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries == None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+#    def save_to_file(cls, list_objs):
+ #       if list_objs == None:
+  #          with open("list_objs.json", "w") as f:
