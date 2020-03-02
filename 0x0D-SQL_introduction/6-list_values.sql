316f7720cbde328035b97db500f87109f1eb4f88
yulyzulu <yulyzulu05@gmail.com>
2020-03-02 20:54:32 +0000

    List all in table

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,2 +1,2 @@
--- Script that prints the full description of the table first_table from the database hbtn_0c_0
-SHOW CREATE TABLE hbtn_0c_0.first_table;
+-- Script that list all rows of the table first_table from the database hbtn_0c_0
+SELECT * FROM first_table;
