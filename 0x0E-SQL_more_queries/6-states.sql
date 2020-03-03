d7ffb57261745c5c73945d186f91289f82a6fb75
yulyzulu <yulyzulu05@gmail.com>
2020-03-03 19:45:26 +0000

    Create database and table with constraint

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,5 @@
 -- Script that creates the database hbtn_0d_usa and the table states..
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa
-CREATE TABLE IF NOT EXISTS states (id INT NOT NULL  AUTO_INCREMENT UNIQUE PRIMARY KEY, name VARCHAR(25, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS states (id INT NOT NULL  AUTO_INCREMENT UNIQUE PRIMARY KEY,
+name VARCHAR(256) NOT NULL);
