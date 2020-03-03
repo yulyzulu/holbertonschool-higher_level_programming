dd5b6e916f7fe67e56fe7bab40d1772105daf526
yulyzulu <yulyzulu05@gmail.com>
2020-03-03 18:13:31 +0000

    First commit

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on server(localhost)
+-- GRANT statement to grant privileges to user accounts.
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+# More Queries# More Queries
