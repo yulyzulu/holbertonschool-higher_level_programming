2db6720117d95411cb78d63eeafa611243a100db
yulyzulu <yulyzulu05@gmail.com>
2020-03-03 19:37:01 +0000

    Create database and table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- Script that creates the database hbtn_0d_usa and the table states..
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT NOT NULL  AUTO_INCREMENT UNIQUE PRIMARY KEY);
