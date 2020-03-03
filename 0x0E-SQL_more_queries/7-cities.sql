dee5745bb2316601b1ca3ace080cdb88db294193
yulyzulu <yulyzulu05@gmail.com>
2020-03-03 21:48:48 +0000

    Subqueri

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
 -- Script that creates the database hbtn_0d_usa and the table cities.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa
-CREATE TABLE IF NOT EXISTS cities (id INT AUTO_INCREMENT NOT NULL UNIQUE,
-PRIMARY KEY(id),
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INT AUTO_INCREMENT NOT NULL,
 state_id INT NOT NULL,
+UNIQUE(id),
+PRIMARY KEY(id),
 FOREIGN KEY(state_id) REFERENCES states(id),
 name VARCHAR(256) NOT NULL);
