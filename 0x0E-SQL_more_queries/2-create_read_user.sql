4b71d3728f587b7a21917d62b667d247ee747650
yulyzulu <yulyzulu05@gmail.com>
2020-03-03 18:52:04 +0000

    Privileges Read User

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,3 +2,7 @@
 -- GRANT statement to grant privileges to user accounts.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+ON hbtn_0d_2
+TO user_0d_2;
+FLUSH PRIVILEGES;
