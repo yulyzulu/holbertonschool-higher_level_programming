64b64a64a0883a77e4efe46306404a66626534af
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-03-28 18:41:06 +0000

    Fix in task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,4 +15,7 @@ st/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     session = Session()
 
     first_state = session.query(State).order_by(State.id).first()
-    print("{}: {}".format(first_state.id, first_state.name))
+    if first_state is not None:
+        print("{}: {}".format(first_state.id, first_state.name))
+    else:
+        print("Nothing")
