79a77ee9c4ff0a9dcd26b246bcf851614d7efc66
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-03-28 19:21:44 +0000

    Fix in 9 task

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,8 +12,7 @@ st/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    session = Session()
+    s = Session()
 
-    for instance in session.query(State).\
-                filter(State.name.like('%a%')).order_by(State.id):
-        print("{:d}: {:s}".format(instance.id, instance.name))
+    for i in s.query(State).filter(State.name.like('%a%')).order_by(State.id):
+        print("{:d}: {:s}".format(i.id, i.name))
