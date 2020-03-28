772d6fd14082d5b8ba11c466ff0c55f78e3b46c8
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-03-28 17:27:37 +0000

    Fixed style pep8

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localho\
 st", user=(sys.argv[1]), port=3306, passwd=(sys.argv[2]), db=(sys.argv[3]))
     c = db.cursor()
-    c.execute("SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON state_id=states.id ORDER BY cities.id ASC;")
+    c.execute("SELECT cities.id,\
+                    cities.name, states.name FROM cities INNER JOIN states\
+                    ON state_id=states.id ORDER BY cities.id ASC;")
     rows = c.fetchall()
     for row in rows:
         print(row)
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,12 +5,21 @@ import MySQLdb
 
 if __name__ == "__main__":
 
-    db = MySQLdb.connect(host="localhost", user=(sys.argv[1]), port=3306, passwd=(sys.argv[2]), db=(sys.argv[3]))
+    db = MySQLdb.connect(host="localho\
+st", user=(sys.argv[1]), port=3306, passwd=(sys.argv[2]), db=(sys.argv[3]))
     c = db.cursor()
-    c.execute("SELECT cities.name FROM cities INNER JOIN states ON state_id=states.id WHERE states.name = %s ORDER BY cities.id ASC;", (sys.argv[4],))
+    c.execute("SELECT cities.name\
+                    FROM cities INNER JOIN states ON state_id=states.id\
+                    WHERE states.name = %s\
+                    ORDER BY cities.id ASC;", (sys.argv[4],))
     rows = c.fetchall()
+    i = 1
     for row in rows:
-        print("{}, ".format(row[0]), end="")
+        if i < len(rows):
+            print("{}, ".format(row[0]), end="")
+            i = i + 1
+        else:
+            print("{}".format(row[0]), end="")
     print()
     c.close()
     db.close()
