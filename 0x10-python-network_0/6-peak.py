76672db51eb627d4f006bae78e5fc8850eefe99b
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-04-09 19:48:31 +0000

    Added -s

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,9 +2,24 @@
 """ Module to execute function"""
 def find_peak(list_of_integers):
     """ Find_peak function"""
-    for i in list_of_integers:
-        if list_of_integers[0] > list_of_integers[1]:
-            return 0
-        if list_of_integers[]
-        if (list_of_integers[i] > list_of_integers[i] - 1 and list_of_integers[i] > list_of_integers[i] + 1):
-            print(i)     
+    len_list = len(list_of_integers)
+    mid = (0 + len_list) / 2
+    if list_of_integers is not None:       
+        for i in range(len(list_of_integers) - 1):
+            if (list_of_integers[mid] > list_of_integers[mid + 1] and list_of_integers[mid] > list_of_integers[mid + 1]):
+                return mid
+
+    else:
+        return None   
+
+
+
+
+
+# for i in range(list_of_integers):
+            #if list_of_integers[0] > list_of_integers[1]:
+            #    return 0
+  #          if (list_of_integers[i] > list_of_integers[i] - 1 and list_of_integers[i] > list_of_integers[i] + 1):
+ #               print(i)     
+   # else:
+    #    return None
