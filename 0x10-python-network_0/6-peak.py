30059a8334d55074de88ba17aa9cf5c3fc5366a4
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-04-13 19:52:11 +0000

    Change sort method

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,7 +5,7 @@
 def find_peak(list_of_integers):
     """ Find_peak function"""
     if list_of_integers:
-        sort_list = sorted(list_of_integers)
-        return sort_list[-1]
+        list_of_integers.sort()
+        return list_of_integers[-1]
     else:
         return None
