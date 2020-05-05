3b0c981261ae80e0b37d4cd7a42c1b6d8c04b8a3
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-05-05 19:18:11 +0000

    Added Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
 
 let x = 'X';
+let num = parseInt(process.argv[2]);
+let i;
+let j;
 
-#!/usr/bin/node
-
-let x = 'X';
-
-
+if (num) {
+  for (i = 0; i < num; i++) {
+    console.log(x.repeat(num));
+  }
+  } else {
+  console.log('Missing size');
+}
