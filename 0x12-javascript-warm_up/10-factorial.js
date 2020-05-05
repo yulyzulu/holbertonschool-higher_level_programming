d011f9d5f9464f88d09bca87935ba88b618ba6cf
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-05-05 18:16:47 +0000

    Update output

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 
-function factorial(num) {
-  if (num < 0) {
-    return -1;
-  } else if (num == 0) {
+
+function factorial(nu) {
+  if (nu === NaN) {
+    return 1;
+  } else if (nu === 0) {
     return 1;
   } else {
-    return (num * factorial(num - 1));
+    return (nu * factorial(nu - 1));
   }
 }
 
-let fact = factorial(process.argv[2]);
+n = process.argv[2];
+let fact = factorial(n);
 console.log(fact);
