f85a0276ce031d844418f56156034a71f14f06c6
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-05-12 16:59:48 +0000

    Added error handled

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,8 @@ const url = process.argv[2];
 const request = require('request');
 
 request(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  }
   console.log('code: ' + res.statusCode);
 });
