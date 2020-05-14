3afd64b5b4fffd0c5ed8346a65f987d89ded514c
Yulieth <yulyzulu>
2020-05-14 16:31:23 -0500

    JQuery project

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,2 @@
+var element = document.querySelector('header');
+element.style.color = '#FF0000';
---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('DIV#toggle_header').on('click', function() {
+        $('header').toggleClass('green red');
+
+    });
+});
